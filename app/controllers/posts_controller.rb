c733963fbb4c852727aadfb6b51b518267a6cfeb
Ubuntu <ubuntu@ip-172-31-31-236.ap-northeast-1.compute.internal>
2026-06-04 01:28:37 +0000

    編集・削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
+
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
   def index
-    # どのブランドの投稿か特定
+    
     @brand = Brand.find(params[:brand_id])
-    # そのブランドの投稿だけを抽出
     @posts = @brand.posts
   end
 
   def new
-    # URLの brand_id からブランドを取得
+    
     @brand = Brand.find(params[:brand_id])
-    # そのブランドに紐づく新しい投稿を作成
     @post = @brand.posts.new
   end
 
@@ -25,8 +26,46 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to brand_posts_path(@post.brand), notice: "投稿を更新しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    # 削除する前にブランド情報を退避しておきます
+    brand = @post.brand
+    @post.destroy
+    redirect_to brand_posts_path(brand), notice: "投稿を削除しました。"
+  end
+
+  def show
+  
+   @post = Post.find(params[:id])
+  end
+
+
   private
+  def ensure_correct_user
+    @post = Post.find(params[:id])
+    # 投稿者とログインユーザーが違ったら一覧へ追い返す
+    unless @post.user == current_user
+      redirect_to brand_posts_path(@post.brand), alert: "権限がありません。"
+    end
+  end
+  
   def post_params
     params.require(:post).permit(:content, :image, :spike_name)
   end
+
+
 end
