b7192d0ca43cc544dba7b5a4e3959bf4e6b24d79
Ubuntu <ubuntu@ip-172-31-31-236.ap-northeast-1.compute.internal>
2026-06-04 05:59:58 +0000

    投稿時のバリデーション

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,20 @@
-# frozen_string_literal: true
-
+# app/controllers/users/sessions_controller.rb
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  # ログインに失敗した際のアクションをカスタマイズ
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  rescue StandardError => e
+    # 空欄の場合などのエラー処理
+    if params[:user][:email].blank? || params[:user][:password].blank?
+      flash.now[:alert] = "メールアドレスまたはパスワードを入力してください"
+      render :new, status: :unprocessable_entity
+    else
+      # 通常の認証失敗（ID/PASS間違い）
+      super
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :brand
   has_one_attached :image
+
+  validates :spike_name, presence: true
+  validates :content, presence: true
+  
 end
