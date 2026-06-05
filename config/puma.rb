8953497d0c1f1a0f989023b2b84719679b0b8155
Ubuntu <ubuntu@ip-172-31-31-236.ap-northeast-1.compute.internal>
2026-06-05 01:08:50 +0000

    deploy setting

--- config/puma.rb
+++ config/puma.rb
@@ -28,7 +28,8 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+#port ENV.fetch("PORT", 3000)
+bind "unix://#{Dir.getwd}/tmp/sockets/puma.sock"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
