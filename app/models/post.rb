27e979953634591ffdeccf7f43df51902fca672e
Ubuntu <ubuntu@ip-172-31-31-236.ap-northeast-1.compute.internal>
2026-06-09 02:56:22 +0000

    検索機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,20 @@ class Post < ApplicationRecord
 
   validates :spike_name, presence: true
   validates :content, presence: true
-  
+
+  def self.search_for(content, method, category)
+    if category == 'name'
+        if method == 'perfect'
+          Post.where(name: content)
+        else
+          Post.where('name LIKE ?', '%' + content + '%')
+        end
+    elsif category == 'position'
+        # ユーザーテーブルと結合（joins）して、ユーザーのポジションを検索する
+        Post.joins(:user).where(users: { position: content })
+    else
+        Post.all
+    end
+  end
 end
+  
