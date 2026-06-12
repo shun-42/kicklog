9cd8b9cc09b1522abe4643a5b2c9d77870a4b7c5
Ubuntu <ubuntu@ip-172-31-31-236.ap-northeast-1.compute.internal>
2026-06-12 02:42:46 +0000

    管理者画面調整１

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,7 +1,13 @@
 class Admin::SessionsController < Devise::SessionsController
- def after_sign_out_path_for(resource_or_scope)
-    new_admin_session_path
- end
+
+  def after_sign_in_path_for(resource)
+    admin_users_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+      new_admin_session_path
+  end
+
 end
   
 
